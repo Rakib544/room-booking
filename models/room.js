0aa46b1b9ba8c2ca49645b530c5775bb9f4578c2
Rakib544 <md.rakib10122003@gmail.com>
2021-09-08 13:27:33 +0600

    room model created

---
+++ models/room.js
@@ -0,0 +1,111 @@
+import mongoose from "mongoose";
+
+const roomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter the room name"],
+    trim: true,
+    maxLength: [100, "Room name cannot be exceed 100 characters"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter the room price"],
+    default: 0.0,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter the room description"],
+  },
+  address: {
+    type: String,
+    required: [true, "Please enter the room address"],
+  },
+  guestCapacity: {
+    type: Number,
+    required: [true, "Please enter the guest capacity"],
+  },
+  numOfBeds: {
+    type: Number,
+    required: [true, "Please enter the number of beds in room"],
+  },
+  internet: {
+    type: Boolean,
+    default: false,
+  },
+  breakfast: {
+    type: Boolean,
+    default: false,
+  },
+  airCondition: {
+    type: Boolean,
+    default: false,
+  },
+  petsAllow: {
+    type: Boolean,
+    default: false,
+  },
+  roomCleaning: {
+    type: Boolean,
+    default: false,
+  },
+  ratings: {
+    type: Number,
+    default: 0,
+  },
+  numOfReviews: {
+    type: Number,
+    default: 0,
+  },
+  images: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  category: {
+    type: String,
+    required: [true, "Please Enter Room Category"],
+    enum: {
+      value: ["King", "single", "Twins"],
+      message: "Please select correct category Room",
+    },
+  },
+  reviews: [
+    {
+      user: {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: {
+        type: String,
+        required: true,
+      },
+      rating: {
+        type: Number,
+        required: true,
+      },
+      comment: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model.Room || mongoose.model("Room", roomSchema);
